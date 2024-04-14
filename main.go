8307241acb2e1316840b3274c6bf057e0fa35b18
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-14 16:04:33 -0500

    feat: index documents

--- main.go
+++ main.go
@@ -14,11 +14,17 @@ func main() {
 
 	log.Println("Text search in progress")
 
+	// Load dump files (documents)
 	start := time.Now()
-
 	docs, err := utils.LoadDocuments(dumpFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
+
+	start = time.Now()
+	idx := make(utils.Index)
+	idx.Add(docs)
+	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
+
 }
