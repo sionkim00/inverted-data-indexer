3d6b8b33e4671ebcff315d90e553dbd9935c8d30
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-14 16:49:46 -0500

    feat: Search for text in tokens

--- utils/index.go
+++ utils/index.go
@@ -6,6 +6,31 @@ func (i Index) Add(docs []document) {
 	for _, doc := range docs {
 		for _, token := range analyze(doc.Text) {
 			ids := i[token]
+
+			if ids != nil && i[len(ids)-1] == doc.ID {
+				// Ignore duplicate ID
+				continue
+			}
+			i[token] = append(ids, doc.ID)
 		}
 	}
+}
+
+func (i Index) Search(text string) []int {
+	var r []int
+
+	for _, token := range analyze(text) {
+		if ids, ok := i[token]; ok {
+			if r == nil {
+				r = ids
+			} else {
+				r = Intersection(r, ids)
+			}
+		} else {
+			// Token does not exist
+			return nil
+		}
+	}
+
+	return r
 }
