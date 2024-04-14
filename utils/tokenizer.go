65614e1ab14999f3c426d898e15324c036a30ddc
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-14 16:43:09 -0500

    feat: tokenize and analyze function

---
+++ utils/tokenizer.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"strings"
+	"unicode"
+)
+
+func tokenize(text string) []string {
+	return strings.FieldsFunc(text, func(r rune) bool {
+		// Check if current token is letter or number
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
+
+func analyze(text string) []string {
+	tokens := tokenize(text)
+	tokens = lowercaseFilter(tokens)
+	tokens = stopwordFilter(tokens)
+	tokens = stemmerFilter(tokens)
+
+	return tokens
+}
