f2b098ec240b19fa92bdf195f6a54e69bd18f23b
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2024-04-14 16:18:15 -0500

    feat: search query from the indexes

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	var dumpFilePath, query string
-	flag.StringVar(&dumpFilePath, "p", "./data/wiki.xml", "wikipedia abstract dump path")
+	flag.StringVar(&dumpFilePath, "p", "./data/wiki.xml.gz", "wikipedia abstract dump path")
 	flag.StringVar(&query, "q", "Members of parliament", "search query")
 	flag.Parse()
 
 	log.Println("Text search in progress")
 
-	// Load dump files (documents)
+	// Take the path and load files (documents) from the path
 	start := time.Now()
 	docs, err := utils.LoadDocuments(dumpFilePath)
 	if err != nil {
@@ -22,9 +22,17 @@ func main() {
 	}
 	log.Printf("Loaded %d documents in %v", len(docs), time.Since(start))
 
+	// Create a new instance of index and send documents to Add function
 	start = time.Now()
 	idx := make(utils.Index)
 	idx.Add(docs)
 	log.Printf("Indexed %d documents in %v", len(docs), time.Since(start))
 
+	// Search the query from the indexes
+	matchingIDs := idx.Search(query)
+	log.Printf("Found %d documents in %v", len(matchingIDs), time.Since(start))
+	for _, id := range matchingIDs {
+		doc := docs[id]
+		log.Printf("%d\t%s\n", id, doc.Text)
+	}
 }
